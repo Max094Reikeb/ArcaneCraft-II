e37e996abee9013d50e219a5ef78f5dea9d0220d
Max094Reikeb <maxencebekier@gmail.com>
2021-07-06 02:15:30 +0200

    Added Crystal tools

--- src/main/java/net/reikeb/arcanecraft/init/ItemInit.java
+++ src/main/java/net/reikeb/arcanecraft/init/ItemInit.java
@@ -22,6 +22,16 @@ public class ItemInit {
     public static final RegistryObject<Item> CRYSTAL_ORE_ITEM = ITEMS.register("crystal_ore", () ->
             new BlockItem(BlockInit.CRYSTAL_ORE.get(), new Item.Properties().tab(ItemGroups.ARCANECRAFT)));
 
+    public static final RegistryObject<Item> CRYSTAL_SWORD = ITEMS.register("crystal_sword", CrystalSword::new);
+
+    public static final RegistryObject<Item> CRYSTAL_PICKAXE = ITEMS.register("crystal_pickaxe", CrystalPickaxe::new);
+
+    public static final RegistryObject<Item> CRYSTAL_AXE = ITEMS.register("crystal_axe", CrystalAxe::new);
+
+    public static final RegistryObject<Item> CRYSTAL_SHOVEL = ITEMS.register("crystal_shovel", CrystalShovel::new);
+
+    public static final RegistryObject<Item> CRYSTAL_HOE = ITEMS.register("crystal_hoe", CrystalHoe::new);
+
     public static final RegistryObject<Item> CRYSTAL_BLOCK_ITEM = ITEMS.register("crystal_block", () ->
             new BlockItem(BlockInit.CRYSTAL_BLOCK.get(), new Item.Properties().tab(ItemGroups.ARCANECRAFT)));
 
---
+++ src/main/java/net/reikeb/arcanecraft/items/CrystalHoe.java
@@ -0,0 +1,58 @@
+package net.reikeb.arcanecraft.items;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.*;
+
+import net.minecraftforge.registries.ForgeRegistries;
+
+import net.reikeb.arcanecraft.init.ItemInit;
+import net.reikeb.arcanecraft.setup.ItemGroups;
+
+public class CrystalHoe extends HoeItem {
+
+    public CrystalHoe() {
+        super(new IItemTier() {
+            @Override
+            public int getUses() {
+                return 187;
+            }
+
+            @Override
+            public float getSpeed() {
+                return 5.5f;
+            }
+
+            @Override
+            public float getAttackDamageBonus() {
+                return -1f;
+            }
+
+            @Override
+            public int getLevel() {
+                return 1;
+            }
+
+            @Override
+            public int getEnchantmentValue() {
+                return 33;
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return Ingredient.of(new ItemStack(ItemInit.CRYSTAL.get(), 1));
+            }
+        }, 1, -3f, new Item.Properties().tab(ItemGroups.ARCANECRAFT));
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack itemstack, LivingEntity entity, LivingEntity sourceentity) {
+        boolean action = super.hurtEnemy(itemstack, entity, sourceentity);
+        SoundEvent grassSound = ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.grass.break"));
+        if (grassSound == null) return false;
+        entity.level.playSound(null, entity.getX(), entity.getY(), entity.getZ(),
+                grassSound, SoundCategory.NEUTRAL, (float) 100, (float) 100);
+        return action;
+    }
+}
