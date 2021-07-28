92bfd5049351321da01a35e61d35001c9588d75a
Max094Reikeb <maxencebekier@gmail.com>
2021-07-28 22:10:23 +0200

    Fixed ManaProvider

--- src/main/java/net/reikeb/arcanecraft/capabilities/ManaProvider.java
+++ src/main/java/net/reikeb/arcanecraft/capabilities/ManaProvider.java
@@ -1,5 +1,6 @@
 package net.reikeb.arcanecraft.capabilities;
 
+import net.minecraft.nbt.INBT;
 import net.minecraft.util.Direction;
 
 import net.minecraftforge.common.capabilities.*;
@@ -7,18 +8,25 @@ import net.minecraftforge.common.util.*;
 
 import javax.annotation.*;
 
-public class ManaProvider<C> implements ICapabilityProvider {
+public class ManaProvider<C, S extends INBT> implements ICapabilityProvider, INBTSerializable<S> {
 
     private final Capability<C> capability;
     private final LazyOptional<C> implementation;
+    private final Direction direction;
 
-    protected ManaProvider(@Nonnull final Capability<C> capability, @Nonnull final LazyOptional<C> implementation) {
+    protected ManaProvider(@Nonnull final Capability<C> capability, @Nonnull final LazyOptional<C> implementation, @Nonnull final Direction direction) {
         this.capability = capability;
         this.implementation = implementation;
+        this.direction = direction;
     }
 
-    public static <C> ManaProvider<C> from(@Nonnull final Capability<C> cap, @Nonnull final NonNullSupplier<C> impl) {
-        return new ManaProvider<>(cap, LazyOptional.of(impl));
+    public static <C> ManaProvider<C, INBT> from(@Nonnull final Capability<C> cap, @Nonnull final NonNullSupplier<C> impl) {
+        return from(cap, null, impl);
+    }
+
+    @Nonnull
+    public static <C> ManaProvider<C, INBT> from(@Nonnull final Capability<C> cap, @Nullable final Direction direction, @Nonnull final NonNullSupplier<C> impl) {
+        return new ManaProvider<>(cap, LazyOptional.of(impl), direction);
     }
 
     @Nonnull
@@ -26,5 +34,22 @@ public class ManaProvider<C> implements ICapabilityProvider {
     public <T> LazyOptional<T> getCapability(@Nonnull final Capability<T> cap, @Nullable final Direction side) {
         if (cap == this.capability) return this.implementation.cast();
         return LazyOptional.empty();
+    }
+
+    @Nullable
+    @Override
+    @SuppressWarnings("unchecked")
+    public S serializeNBT() {
+        return (S) this.capability.writeNBT(this.getInstance(), this.direction);
+    }
+
+    @Override
+    public void deserializeNBT(@Nonnull final S nbt) {
+        this.capability.readNBT(this.getInstance(), this.direction, nbt);
+    }
+
+    @Nonnull
+    private C getInstance() {
+        return this.implementation.orElseThrow(() -> new IllegalArgumentException("Unable to obtain capability instance"));
     }
 }
