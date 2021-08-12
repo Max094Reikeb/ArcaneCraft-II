db78f2e569c472d865001c469c9689221eb3e0ed
Max094Reikeb <maxencebekier@gmail.com>
2021-08-12 12:40:19 +0200

    Modified Amethyst Hammer and added new Tier

--- src/main/java/net/reikeb/arcanecraft/items/AmethystHammer.java
+++ src/main/java/net/reikeb/arcanecraft/items/AmethystHammer.java
@@ -8,8 +8,9 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.*;
-import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.DiggerItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -22,6 +23,7 @@ import net.minecraftforge.common.ToolAction;
 import net.minecraftforge.common.ToolActions;
 
 import net.reikeb.arcanecraft.ArcaneCraft;
+import net.reikeb.arcanecraft.misc.Tiers;
 import net.reikeb.arcanecraft.setup.ItemGroups;
 import net.reikeb.arcanecraft.utils.Util;
 
@@ -33,43 +35,14 @@ public class AmethystHammer extends DiggerItem {
     private static final Tags.IOptionalNamedTag<Block> MINEABLE_WITH_HAMMER = BlockTags.createOptional(ArcaneCraft.RL("mineable/hammer"));
 
     public AmethystHammer() {
-        super(1F, -2.8F, new Tier() {
-            @Override
-            public int getUses() {
-                return 512;
-            }
-
-            @Override
-            public float getSpeed() {
-                return 6.0F;
-            }
-
-            @Override
-            public float getAttackDamageBonus() {
-                return 2.0F;
-            }
-
-            @Override
-            public int getLevel() {
-                return 3;
-            }
-
-            @Override
-            public int getEnchantmentValue() {
-                return 15;
-            }
-
-            @Override
-            public Ingredient getRepairIngredient() {
-                return Ingredient.of(new ItemStack(Items.AMETHYST_SHARD, 1));
-            }
-        }, MINEABLE_WITH_HAMMER, new Item.Properties().tab(ItemGroups.ARCANECRAFT));
+        super(1F, -2.8F, Tiers.AMETHYST_TIER, MINEABLE_WITH_HAMMER,
+                new Item.Properties().tab(ItemGroups.ARCANECRAFT));
     }
 
     @Override
     public boolean canPerformAction(ItemStack stack, ToolAction toolAction) {
         return (toolAction.equals(ToolActions.SHOVEL_DIG) || (toolAction.equals(ToolActions.AXE_DIG)) ||
-                (toolAction.equals(ToolActions.PICKAXE_DIG)));
+                (toolAction.equals(ToolActions.PICKAXE_DIG)) || (toolAction.equals(ToolActions.HOE_DIG)));
     }
 
     public boolean mineBlock(ItemStack stack, Level world, BlockState state, BlockPos pos, LivingEntity entity) {
