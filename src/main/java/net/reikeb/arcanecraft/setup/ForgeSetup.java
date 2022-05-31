0db518ce275909a031e59469bf4437127e90ede3
Max094Reikeb <maxencebekier@gmail.com>
2022-05-31 11:06:00 +0200

    Removed Mana command

--- src/main/java/net/reikeb/arcanecraft/setup/ForgeSetup.java
+++ src/main/java/net/reikeb/arcanecraft/setup/ForgeSetup.java
@@ -1,33 +1,20 @@
 package net.reikeb.arcanecraft.setup;
 
-import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.reikeb.arcanecraft.ArcaneCraft;
 import net.reikeb.arcanecraft.capabilities.CapabilityMana;
 import net.reikeb.arcanecraft.capabilities.ManaProvider;
 import net.reikeb.arcanecraft.capabilities.ManaStorage;
-import net.reikeb.arcanecraft.commands.ManaCommand;
 import net.reikeb.arcanecraft.misc.Keys;
 
 import javax.annotation.Nonnull;
 
 @Mod.EventBusSubscriber(modid = ArcaneCraft.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeSetup {
-
-    /**
-     * Register commands
-     */
-    @SubscribeEvent
-    public static void onRegisterCommandEvent(RegisterCommandsEvent event) {
-        CommandDispatcher<CommandSourceStack> commandDispatcher = event.getDispatcher();
-        ManaCommand.register(commandDispatcher);
-    }
 
     /**
      * Attach capabilities to players
